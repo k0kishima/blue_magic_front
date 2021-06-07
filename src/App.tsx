1191f2c644b67d00ed39475c628030ec67ac05cb
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-07 20:36:51 +0900

    scaffold a router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { DashboardContainer } from "containers/DashboardContainer";
+import { SettingsContainer } from "containers/SettingsContainer";
 
 export const App: React.VFC = () => {
-  return <DashboardContainer />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {DashboardContainer}
+          </Route>
+          <Route path="/settings">{SettingsContainer}</Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
---
+++ src/containers/SettingsContainer/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SettingsContainer: React.VFC = () => {
+  return <div>implement a settings feature here.</div>;
+};
