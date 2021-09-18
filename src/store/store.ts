e1223d731212c5b79cdafe126473c30a919cdff4
Johndoe <56298669+togane@users.noreply.github.com>
2021-09-18 16:12:42 +0900

    install and enable redux-localstorage-simple

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "store/rootReducer";
 import { settingApi } from "services/settings";
+import { save, load } from "redux-localstorage-simple";
 
 export const store = configureStore({
   reducer: rootReducer,
+  preloadedState: load(),
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(settingApi.middleware),
+    getDefaultMiddleware().concat([save(), settingApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
