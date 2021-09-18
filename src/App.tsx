f9b81664997d4c5734236a063b348c610ecb8e08
Johndoe <56298669+togane@users.noreply.github.com>
2021-09-18 10:03:30 +0900

    enable reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { DashboardPage } from "components/pages/DashboardPage";
 import { SettingsPage } from "components/pages/SettingsPage";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`;
 
 export const App: React.VFC = () => {
   return (
     <div>
+      <GlobalStyle />
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
