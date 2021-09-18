f2c55a8aaf4c2aa48d6e1441efe1133b1568c540
Johndoe <56298669+togane@users.noreply.github.com>
2021-09-18 17:37:13 +0900

    stop to save store states to localstrage temporarily

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "store/rootReducer";
 import { settingApi } from "services/settings";
-import { save, load } from "redux-localstorage-simple";
 
 export const store = configureStore({
   reducer: rootReducer,
-  preloadedState: load(),
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([save(), settingApi.middleware]),
+    getDefaultMiddleware().concat(settingApi.middleware),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
